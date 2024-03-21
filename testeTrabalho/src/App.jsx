eab9e5d62a649a1aa30eacd6bde688f7f1f90e2b
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-03-21 07:58:25 -0300

    teste

---
+++ testeTrabalho/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>gay?</h1>
+      </header>
+      <section>
+      <img src="./aafolou.jpg"/>
+      </section>
+    </>
+  );
+}
+
+export default App;
